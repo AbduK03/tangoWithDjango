e7a8f0013b0561434f3436e425dff9b4765fcd3a
AbduK03 <abdurahmankhan2003@icloud.com>
2022-03-22 15:59:47 +0000

    gitignore

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -21,6 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #template path
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+#static path
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -128,3 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
