01ba8688438fb3e2f12e9881b2551ae17814bb2f
AbduK03 <abdurahmankhan2003@icloud.com>
2022-03-22 19:29:54 +0000

    my brain is about to explode...static page and beginings of the app

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page , courseTitle ,startDate , desc
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(courseTitle)
+admin.site.register(startDate)
+admin.site.register(desc)
+
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+
 
         def __str__(self):
             return self.name
@@ -23,3 +26,48 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class courseTitle(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Course Title'
+
+
+        def __str__(self):
+            return self.name
+
+
+    def __str__(self):
+        return self.name
+
+class startDate(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    date = models.IntegerField(default=2022)
+
+
+    class Meta:
+        verbose_name_plural = 'Start Date'
+
+
+        def __str__(self):
+            return self.name
+
+
+    def __str__(self):
+        return self.name
+
+class desc(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+    class Meta:
+        verbose_name_plural = 'Description'
+
+
+        def __str__(self):
+            return self.name
+
+
+    def __str__(self):
+        return self.name
